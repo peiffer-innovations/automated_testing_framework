70a3431c5357d13e06a058c4dde66b27160f716e
root <noop@github.com>
2022-12-27 23:00:37 +0000

    Pages 94

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -33,8 +33,8 @@ const RESOURCES = {
 "assets/FontManifest.json": "3a1c01ebd9adb7aecf384a895a3cbb43",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
 "assets/NOTICES": "c3c726db202f26a59a1e74dbcf45518e",
-"index.html": "3014a80c805fa3883feb9a97e6a8f9db",
-"/": "3014a80c805fa3883feb9a97e6a8f9db",
+"index.html": "80d1407d77b35e4f8b46b66b8c761798",
+"/": "80d1407d77b35e4f8b46b66b8c761798",
 "favicon.png": "7d2009652f165b0bc9d9e23452621c35",
 "version.json": "7fd137b2a5c2fdd6cc43cc0d7ee0cd4d"
 };
