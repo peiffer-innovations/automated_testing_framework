b6f8649fb4881954c2077eef9462ac2b560c2b28
action <noop@github.com>
2023-01-31 02:27:57 +0000

    Pages 103

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -4,8 +4,8 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "flutter.js": "1cfe996e845b3a8a33f57607e8b09ee4",
-"index.html": "aa186701c4a2e3df1baefabdfc9be8c7",
-"/": "aa186701c4a2e3df1baefabdfc9be8c7",
+"index.html": "a09c1669ce2d61108e8744bff9c9a9fb",
+"/": "a09c1669ce2d61108e8744bff9c9a9fb",
 "main.dart.js": "c21083c0a92c3df892c5993e29cb5282",
 "manifest.json": "15f73b7e8a8209c2206210b3ac8dea1b",
 "favicon.png": "7d2009652f165b0bc9d9e23452621c35",
@@ -35,7 +35,7 @@ const RESOURCES = {
 "canvaskit/canvaskit.js": "97937cb4c2c2073c968525a3e08c86a3",
 "canvaskit/profiling/canvaskit.wasm": "371bc4e204443b0d5e774d64a046eb99",
 "canvaskit/profiling/canvaskit.js": "c21852696bc1cc82e8894d851c01921a",
-"version.json": "7fd137b2a5c2fdd6cc43cc0d7ee0cd4d"
+"version.json": "85992486dcf941ce52040f7bd0fb0f23"
 };
 
 // The application shell files that are downloaded before a service worker can
